591642cef9d58f9e378e108d4450fdf37590f09e
VladusDev <vlad.ssap@gmail.com>
2024-03-19 16:47:58 +0300

    photo-library

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "./index.scss";
+import Collection from "./Collection";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const cats = [
+  { name: "Все" },
+  { name: "Море" },
+  { name: "Горы" },
+  { name: "Архитектура" },
+  { name: "Города" },
+];
+
+function App() {
+  const [categoryid, setCategoryID] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
+  const [collections, setCollections] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+
+    const category = categoryid ? `category=${categoryid}` : "";
+    fetch(
+      `https://65f96210df15145246117346.mockapi.io/api/v-v1/Collections?page=${page}&limit=3&${category}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении данных");
+      })
+      .finally(() => setIsLoading(false));
+  }, [categoryid, page]);
+
+  return (
+    <div className="App">
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {cats.map((obj, i) => (
+            <li
+              onClick={() => setCategoryID(i)}
+              className={categoryid === i ? "active" : ""}
+              key={obj.name}
+            >
+              {obj.name}
+            </li>
+          ))}
+        </ul>
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
+      </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Идёт загрузка...</h2>
+        ) : (
+          collections
+            .filter((obj) => {
+              return obj.name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map((obj, index) => (
+              <Collection key={obj.index} name={obj.name} images={obj.photos} />
+            ))
+        )}
+      </div>
+      <ul className="pagination">
+        {[...Array(3)].map((_, i) => (
+          <li
+            onClick={() => setPage(i + 1)}
+            className={page === i + 1 ? "active" : ""}
+          >
+            {i + 1}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
